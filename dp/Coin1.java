e9d1cd54fb332626b95bb15b2cf7e0bdb2b12457
Yeonjin Kim <jinny309@gmail.com>
2019-04-10 17:32:20 +0900

    [백준] 동전 2 - 수정

--- dp/Coin1.java
+++ dp/Coin1.java
@@ -16,7 +16,7 @@ public class Coin1 {
 		k = sc.nextInt();
 		coins = new int[n+1];
 		for(int i=0; i<n; i++)
-			coins[i] = sc.nextInt();
+		    coins[i] = sc.nextInt();
 		sc.close();
 		
 		// calculate
@@ -24,10 +24,10 @@ public class Coin1 {
 		Dp[0] = 1;
 		// i번째 동전까지 사용
 		for (int i=0; i<n; i++) {
-			// j라는 숫자를 만듦
-			for (int j=1; j<=k; j++) {
-		        	if (j >= coins[i]) {
-		        		Dp[j] += Dp[j-coins[i]];
+		    // j라는 숫자를 만듦
+		    for (int j=1; j<=k; j++) {
+		        if (j >= coins[i]) {
+		            Dp[j] += Dp[j-coins[i]];
 		        }
 		    }
 		}
