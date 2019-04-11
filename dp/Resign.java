603078d728247bd3eecf6143330db2cfc74cbca6
Yeonjin Kim <jinny309@gmail.com>
2019-04-11 17:26:14 +0900

    [백준] 퇴사 - 수정
    설명을 위한 주석 추가

--- dp/Resign.java
+++ dp/Resign.java
@@ -17,11 +17,16 @@ public class Resign {
         
         dp = new int[N+2];
         max = 0;
+        // 며칠을 사용해야 하는지에 따라 계산하기 때문에 마지막 날부터 거꾸로 계산해나간다.
         for(int day=N; day>0; day--) {
+            // 예외처리
         	if(day+schedule[day][0]>N+1) {
         		if(day<N) dp[day] = dp[day+1];
         		continue;
         	}
+            // 계산
+            // - 오늘 상담을 하지 않거나(다음 날의 값과 같다)
+            // - 오늘 상담을 하고 schedule[day][0]일 후에 다시 상담을 재개
         	dp[day] = Math.max(dp[day+1], dp[day+schedule[day][0]] + schedule[day][1]);
         	max = Math.max(max, dp[day]);
         }
