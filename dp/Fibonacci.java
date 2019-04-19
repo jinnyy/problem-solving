e2c887947d5287b6ee77e0991f404bdf7676017e
Yeonjin Kim <jinny309@gmail.com>
2019-04-19 15:00:18 +0900

    [백준] 피보나치 함수 - 생성

---
+++ dp/Fibonacci.java
@@ -0,0 +1,31 @@
+package dp;
+/*
+ * [백준] 피보나치 함수
+ * https://www.acmicpc.net/problem/1003
+ */
+import java.util.Scanner;
+
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int T, N, dp[][];
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		dp = new int[41][2];
+    
+		dp[0] = new int[] {1, 0};
+		dp[1] = new int[] {0, 1};
+		for(int i=2; i<=40; i++) {
+			for(int j=0; j<2; j++)
+				dp[i][j] = dp[i-1][j] + dp[i-2][j];
+		}
+		
+    // print
+		for(int testCase=0; testCase<T; testCase++) {
+			N = sc.nextInt();
+			System.out.println(String.format("%s %s", dp[N][0], dp[N][1]));
+		}
+		sc.close();	
+	}
+}
