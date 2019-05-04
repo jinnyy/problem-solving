394f6082d799e42950546d1214b96e90e1ce933c
Yeonjin Kim <jinny309@gmail.com>
2019-05-04 21:23:18 +0900

    [백준] 이항 계수 2

---
+++ dp/BinomialCoefficient2.java
@@ -0,0 +1,34 @@
+package dp;
+/*
+ * [백준] 이항 계수 2
+ * https://www.acmicpc.net/problem/11051
+ */
+import java.util.Scanner;
+
+
+public class BinomialCoefficient2 {
+	static final int MOD = 10007;
+
+	public static void main(String[] args) {
+		int N, K, pascal[][];
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		sc.close();
+		
+		pascal = new int[1001][1001];
+		for(int i=0; i<=N; i++) {
+			for(int j=0; j<=i; j++) {
+				if(j>i/2)
+					pascal[i][j] = pascal[i][i-j];
+				else {
+					if(j==0) pascal[i][j] = 1;
+					else pascal[i][j] = (pascal[i-1][j-1] + pascal[i-1][j]) % MOD;
+				}
+			}
+		}
+		
+		System.out.println(pascal[N][K]);
+	}
+
+}
