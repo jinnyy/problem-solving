c5a73feea89f7eade9a74b3739a91341b7932908
Yeonjin Kim <jinny309@gmail.com>
2019-05-29 02:08:17 +0900

    [백준] 동전 2

---
+++ dp/Coin2.java
@@ -0,0 +1,39 @@
+package dp;
+/*
+ * [백준] 동전 2
+ * https://www.acmicpc.net/problem/2294
+ */
+import java.util.Scanner;
+
+
+public class Coin2 {
+	public static void main(String[] args) {
+		int n, k, coins[], dp[];
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		k = sc.nextInt();
+		coins = new int[n];
+		dp = new int[k+1];
+		for(int i=0; i<n; i++) {
+			coins[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		for(int coin: coins) {
+			if(coin>k) continue;
+			dp[coin] = 1;
+		}
+		
+		for(int i=1; i<=k; i++) {
+			for(int coin: coins) {
+				if(i-coin<0 || dp[i-coin]==0) continue;
+				if(dp[i]==0)
+					dp[i] = dp[i-coin] + 1;
+				else
+					dp[i] = Math.min(dp[i], dp[i-coin]+1);
+			}
+		}
+		System.out.println(dp[k]==0? -1: dp[k]);
+	}
+
+}
