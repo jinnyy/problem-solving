2870f6beccfe98966409f7571e944dc9fe33fb36
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-07-15 17:07:50 +0900

    [프로그래머스] [DFS/BFS] 네트워크

---
+++ Programmers/Network.java
@@ -0,0 +1,34 @@
+/*
+ * [프로그래머스] [DFS/BFS] 네트워크
+ * https://programmers.co.kr/learn/courses/30/lessons/43162
+ */
+package programmers;
+
+
+public class Network {
+	static int cnt, N;
+	
+	// dfs로 연결 요소의 개수를 찾는다.
+	public int solution(int n, int[][] computers) {
+		cnt = 0;
+		N = n;
+		boolean[] visited = new boolean[n];
+		for(int i=0; i<N; i++) {
+			// 새로운 노드에서 시작할 때 cnt를 1씩 증가시킨다.
+			if(!visited[i]) {
+				dfs(computers, visited, i);
+				cnt++;
+			}
+		}
+        return cnt;
+    }
+	
+	private static void dfs(int[][] computers, boolean[] visited, int cur) {
+		visited[cur] = true;
+		for(int i=0; i<N; i++) {
+			if(!visited[i] && computers[cur][i]==1 && i!=cur) {
+				dfs(computers, visited, i);
+			}
+		}
+	}
+}
