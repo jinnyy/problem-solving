8b52089c3ede5c92d11d138f34b01c1908984d05
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-07-17 15:32:54 +0900

    [프로그래머스][DP] 타일 장식

---
+++ Programmers/TileDecoration.java
@@ -0,0 +1,21 @@
+package programmers;
+
+
+public class TileDecoration {
+	public static void main(String[] args) {
+		System.out.println(solution(5));
+	}
+
+	public static long solution(int N) {
+		// dp[i] : i번째 사각형의 변의 길이
+		long[] dp = new long[81];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 1;
+		for(int i=3; i<=N; i++) {
+			  dp[i] = dp[i-1] + dp[i-2];
+		}
+		// 사각형의 둘레의 길이를 리턴
+		return (dp[N-1] + 2 * dp[N]) * 2;
+    }
+}
