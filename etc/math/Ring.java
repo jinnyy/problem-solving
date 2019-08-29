30b9365646ee5c7737eaca8474bc443165454cc3
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-08-29 15:03:58 +0900

    [백준][3036] 링

---
+++ etc/math/Ring.java
@@ -0,0 +1,41 @@
+/*
+ * [백준][3036] 링
+ * 13120 KB	80 ms
+ */
+package etc;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Ring {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int first = Integer.parseInt(st.nextToken());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=1; i<N; i++) {
+			int cur = Integer.parseInt(st.nextToken());
+			int G = gcd(first, cur);
+			// write
+			StringBuilder sb = new StringBuilder();
+			sb.append(first/G);
+			sb.append("/");
+			sb.append(cur/G);
+			bw.append(sb.toString());
+			bw.newLine();
+		}
+		br.close();
+		bw.close();
+	}
+	
+	private static int gcd(int A, int B) {
+		if(B==0) return A;
+		return gcd(B, A%B);
+	}
+
+}
