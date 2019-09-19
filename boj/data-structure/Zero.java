cd4381b7c4ef7f25f8a15a32b39d16f02ad1b70f
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-09-19 15:57:02 +0900

    [백준][10773] 제로 - 제출 형식에 맞게 수정

--- boj/data-structure/Zero.java
+++ boj/data-structure/Zero.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
-public class Zero {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int K = Integer.parseInt(br.readLine());
