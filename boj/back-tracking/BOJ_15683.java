e33f2e9f11aff369393e1d7e6b2664eaaac7181c
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-10-18 18:44:49 +0900

    [백준][15683] 감시 - 메모리, 시간 단축

--- boj/back-tracking/BOJ_15683.java
+++ boj/back-tracking/BOJ_15683.java
@@ -1,6 +1,6 @@
 /*
  * [백준][15683] 감시
- * 70840 KB	360 ms
+ * 32472 KB	300 ms
  */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,12 +9,13 @@ import java.util.ArrayList;
 
 
 public class Main {
-	static final int WALL=-1, WATCHING=-2;
-	static final int[] dy = {0, -1, 0, 1};	// 우 상 좌 하
-	static final int[] dx = {1, 0, -1, 0};
-	static int N, M, map[][], min;
+	static final int WALL = -1;
+	static final int MAX = 65;
+	static final int[] dy = {0, -1, 0, 1};
+	static final int[] dx = {-1, 0, 1, 0};
+	static int N, M, map[][], C, min;
 	static ArrayList<Camera> cameras;
-	
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -26,96 +27,107 @@ public class Main {
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int j=0; j<M; j++) {
 				int cur = Integer.parseInt(st.nextToken());
-				int val = 0;
-				if(cur == 6) val = -1;
-				else if(cur == 1) val = 1;
-				else if(cur == 2) val = 5;
-				else if(cur == 3) val = 3;
-				else if(cur == 4) val = 7;
-				else if(cur == 5) val = 15;
-				map[i][j] = val;
-				if(map[i][j] > 0)
-					cameras.add(new Camera(i, j));
+				if(cur == 6) cur = WALL;
+				else if(cur == 0) cur = 0;
+				else {
+					cameras.add(new Camera(i, j, cur));
+					if(cur == 1) cur = 2;			// [1] - - 우 -
+					else if(cur == 2) cur = 10;		// [2] 좌 - 우 -
+					else if(cur == 3) cur = 6;		// [3] - 상 우 -
+					else if(cur == 4) cur = 14;		// [4] 좌 상 우 -
+					else if(cur == 5) cur = 15;		// [5] 좌 상 우 하
+					cameras.get(cameras.size()-1).dir = cur;
+				}
+				map[i][j] = cur;
 			}
 		}
 		br.close();
-		min = 64;
-		go(0);
+		C = cameras.size();
+		
+		min = MAX;
+		select(0, new int[C]);
 		System.out.print(min);
 	}
 	
-	private static void watch() {
-		for(int i=0; i<cameras.size(); i++) {
-			Camera cam = cameras.get(i);
-			int camdir = map[cam.y][cam.x];
-			for(int dir=0; dir<4; dir++) {
-				int localDir = camdir >> dir;
-				if(localDir % 2 == 1) {
-					// 표시
-					int y = cam.y + dy[dir];
-					int x = cam.x + dx[dir];
-					while(y>=0 && x>=0 && y<N && x<M) {
-						if(map[y][x] == WALL) break;
-						if(map[y][x]==0) map[y][x] = WATCHING;
-						y += dy[dir];
-						x += dx[dir];
-					}
-				}
-			}
-			
-		}
-	}
-	
-	private static void go(int depth) {
-		if(depth == cameras.size()) {
-			int[][] map_cache = new int[N][M];
-			for(int i=0; i<N; i++) {
-				map_cache[i] = map[i].clone();
-			}
-			// 카메라 바라보는 방향 표시
-			watch();
-			
-			// 세기
-			int cnt = 0;
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<M; j++) {
-					if(map[i][j] == 0) cnt++;
-				}
-			}
-			if(cnt < min)
-				min = cnt;
-			// 초기화
-			map = map_cache;
+	// 0번 cctv --> N-1번 cctv까지 (각각 방향 선택)
+	static void select(int depth, int[] dirs) {
+		if(depth == C) {
+			boolean[][] visited = visit(dirs);
+			min = Math.min(min, count(visited));
 			return;
 		}
 		
-		// backTracking
+		// 방향 선택
 		Camera cur = cameras.get(depth);
-		int prev = map[cur.y][cur.x];
-		if(prev == 15) {
-			go(depth+1);
-		} else if(prev == 5) {
-			go(depth+1);
-			map[cur.y][cur.x] = 10;
-			go(depth+1);
-			map[cur.y][cur.x] = prev;
+		if(cur.type == 5) {
+			dirs[depth] = cur.dir;
+			select(depth+1, dirs);
+		} else if(cur.type == 2) {
+			// 방향 2개
+			dirs[depth] = cur.dir;
+			select(depth+1, dirs);
+			dirs[depth] = rotate(cur.dir);
+			select(depth+1, dirs);
+			dirs[depth] = 0;
 		} else {
+			int ndir = cur.dir;
 			for(int i=0; i<4; i++) {
-				int next = (prev << i) % 16;
-				next += prev >> (4-i);
-				map[cur.y][cur.x] = next;
-				go(depth+1);
-				map[cur.y][cur.x] = prev;
+				dirs[depth] = ndir;
+				select(depth+1, dirs);
+				dirs[depth] = 0;
+				ndir = rotate(ndir);
 			}
 		}
 	}
+	
+	private static int count(boolean[][] visited) {
+		int cnt = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(!visited[i][j] && map[i][j]==0)
+					cnt++;
+			}
+		}
+		return cnt;
+	}
+	
+	private static boolean[][] visit(int[] dirs) {
+		boolean[][] visited = new boolean[N][M];
+		for(int c=0; c<C; c++) {
+			Camera cam = cameras.get(c);
+			int dir = dirs[c];
+			// 모든 방향 방문
+			for(int i=0; i<4; i++) {
+				if(dir%2 == 1) {
+					int cy = cam.y;
+					int cx = cam.x;
+					while(cy>=0 && cx>=0 && cy<N && cx<M) {
+						if(map[cy][cx]==WALL) break;
+						visited[cy][cx] = true;
+						cy += dy[i];
+						cx += dx[i];
+					}
+				}
+				dir = dir >> 1;
+			}
+		}
+		return visited;
+	}
+	
+	
+	private static int rotate(int dir) {
+		return (dir << 1) % 16 + dir/8;
+	}
+
 }
 
+
 class Camera {
-	int y, x;
+	int y, x, type, dir;
 	
-	Camera(int y, int x){
+	Camera(int y, int x, int type) {
 		this.y = y;
 		this.x = x;
+		this.type = type;
 	}
 }
