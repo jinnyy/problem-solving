cc796273a387a4bab9ff26c257661a1dffd94f88
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-10-18 23:40:32 +0900

    [백준][17142] 연구소 3

--- boj/graph/dfs-bfs/BOJ_17142.java
+++ boj/graph/dfs-bfs/BOJ_17142.java
@@ -1,18 +1,23 @@
 /*
  * [백준][17142] 연구소 3
- * 41148 KB	220 ms
+ * 41980 KB	192 ms
  */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
+
 public class Main {
+	static final int EMPTY = 0;
+	static final int WALL = 1;
+	static final int VIRUS = 2;
+	static final int MAX = 2500;
 	static final int[] dy = {-1, 1, 0, 0};
 	static final int[] dx = {0, 0, -1, 1};
-	static final int MAX = 50 * 50;
-	static final int WALL = 1;
-	static int N, M, map[][], min, goal;
+	static int N, M, map[][], V, min, startCnt;
+	static ArrayList<int[]> Viruses;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,77 +25,64 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][N];
+		startCnt = 0;
+		Viruses = new ArrayList<int[]>();
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int j=0; j<N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j]==0) goal++;
+				if(map[i][j] == VIRUS) Viruses.add(new int[] {i, j});
+				if(map[i][j] != WALL) startCnt++;
 			}
 		}
 		br.close();
-		
+		V = Viruses.size();
 		min = MAX;
-		select(0, 0, -1, new int[M][2], new boolean[N][N]);
+		select(0, -1, new int[M]);
 		
-		if(min == MAX) System.out.print(-1);
-		else System.out.print(min);
+		System.out.print(min==MAX ? -1 : min);
 	}
 	
-	static void select(int depth, int cy, int cx, int[][] seq, boolean[][] visited) {
+	
+	static void select(int depth, int cur, int[] selected) {
 		if(depth == M) {
-			min = Math.min(min, bfs(seq));
+			min = Math.min(min, bfs(selected));
 			return;
 		}
-		for(int i=cy; i<N; i++) {
-			int start = i==cy ? cx+1 : 0;
-			for(int j=start; j<N; j++) {
-				if(map[i][j]!=2 || visited[i][j]) continue;
-				visited[i][j] = true;
-				seq[depth] = new int[] {i, j};
-				select(depth+1, i, j, seq, visited);
-				seq[depth] = new int[2];
-				visited[i][j] = false;
-			}
+		
+		for(int next=cur+1; next<V; next++) {
+			selected[depth] = next;
+			select(depth+1, next, selected);
+			selected[depth] = 0;
 		}
 	}
 	
-	static int bfs(int[][] viruses) {
-		LinkedList<Virus> Q = new LinkedList<Virus>();
-		boolean[][] visited = new boolean[N][N];
-		for(int[] virus : viruses) {
-			Q.add(new Virus(virus[0], virus[1], 0));
-			visited[virus[0]][virus[1]] = true;
+	static int bfs(int[] selected) {
+		int time = 1;
+		int cnt = startCnt;
+		LinkedList<int[]> Q = new LinkedList<int[]>();
+		int[][] timeMap = new int[N][N];
+		for(int i=0; i<M; i++) {
+			int[] v = Viruses.get(selected[i]);
+			Q.add(v);
+			timeMap[v[0]][v[1]] = 1;
 		}
 		
-		int time = 0;
-		int cnt = 0;
 		while(!Q.isEmpty()) {
-			Virus cur = Q.poll();
-			time = Math.max(time, cur.time);
-			if(map[cur.y][cur.x] == 0) cnt++;
-			if(cnt >= goal) break;
+			int[] cur = Q.poll();
+			cnt--;
 			
-			for(int d=0; d<4; d++) {
-				int ny = cur.y + dy[d];
-				int nx = cur.x + dx[d];
-				if(ny<0 || nx<0 || ny>=N || nx>=N) continue;
-				if(!visited[ny][nx] && map[ny][nx]!=WALL) {
-					visited[ny][nx] = true;
-					Q.add(new Virus(ny, nx, cur.time+1));
-				}
+			for(int i=0; i<4; i++) {
+				int ny = cur[0] + dy[i];
+				int nx = cur[1] + dx[i];
+				if(ny<0 || nx<0 || ny>=N || nx>=N || map[ny][nx]==WALL || timeMap[ny][nx]>0) continue;
+				timeMap[ny][nx] = timeMap[cur[0]][cur[1]] + 1;
+				if(map[ny][nx] != VIRUS)
+					time = Math.max(time, timeMap[ny][nx]);
+				Q.add(new int[] {ny, nx});
 			}
 		}
-		if(cnt >= goal) return time;
-		else return MAX;
-	}
-}
-
-class Virus {
-	int y, x, time;
-	
-	Virus(int y, int x, int time){
-		this.y = y;
-		this.x = x;
-		this.time = time;
+		
+		return cnt==0 ? time - 1 : MAX;
 	}
 }
