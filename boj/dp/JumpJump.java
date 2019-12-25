51ce858781c3b1025737b3052cae3fd6162a512e
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-12-25 20:05:14 +0900

    [백준] 점프점프

--- boj/dp/JumpJump.java
+++ boj/dp/JumpJump.java
@@ -2,11 +2,10 @@
  * [백준] 점프점프
  * 11060
  */
-package dp;
 import java.util.Scanner;
 
 
-public class JumpJump {
+public class Main {
 
 	public static void main(String[] args) {
 		int N, A[], dp[];
